75f87cbe4c5827887b303ae0e47cda007c188c36
AleshaRP <createlele@yahoo.com>
2018-06-21 18:40:37 +0000

    automatically backed up by learn

--- recipe_hash.rb
+++ recipe_hash.rb
@@ -1,14 +1,14 @@
-perfect_10_recipe = {
- :almondflour => "2 cups",
- :glutenfreewholeoats => "1 cup",
- :koshersalt => "1/2 tea spoon",
- :bakingpowder => "1/2 tea spoon",
- :bakingsoda => "1/4 tea spoon",
- :XanthanGum => "1/2 tea spoon",
- :sliveredalmondslightlytoasted => "1/4 cup",
- :minidarkchocolatechips => "3/4 cup",
- :oliveoil => "1/2 cup",
- :agave => "1/4 cup"
+perfect_10_recipe = { 
+:almondflour => "2 cups",
+:glutenfreewholeoats => "1 cup",
+:koshersalt => "1/2 tea spoon",
+:bakingpowder => "1/2 tea spoon",
+:bakingsoda => "1/4 tea spoon",
+:XanthanGum => "1/2 tea spoon",
+:sliveredalmondslightlytoasted => "1/4 cup",
+:minidarkchocolatechips => "3/4 cup",
+:oliveoil => "1/2 cup",
+:agave => "1/4 cup"
 }
 
 
